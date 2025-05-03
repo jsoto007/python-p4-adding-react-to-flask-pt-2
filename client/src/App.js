5208dacc0074359e86c5edb9b51c7244cd36beab
jsoto007 <jsoto7087@gmail.com>
2025-05-03 19:27:50 -0400

    fontend fetch with no errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [films, setFilms] = useState([]);
+
+  useEffect(() => {
+    fetch('/movies')
+      .then((resp) => resp.json())
+      .then((movies) => setFilms(movies));
+
+  }, []);
+console.log(films)
+return (
+  <div>
+    {films.map((movie) => {
+      return (
+        <ul key={movie.id}>
+          <li>{movie.id}</li>
+          <li>{movie.title}</li>
+        </ul>
+      )
+    })}
+  </div>
+  )
+
 }
 
 
 export default App;
+
